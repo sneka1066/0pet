50e0aa700a57d6cb5d586af0ecb71b4c33899949
Abimanyu M <131531420+abimanyu-2001@users.noreply.github.com>
2024-07-18 05:42:14 -0700

    Add files via upload

---
+++ Abimanyu/AdminHead.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "../styles/admin.css";
+
+const AdminHead = () => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+};
+
+export default AdminHead;
